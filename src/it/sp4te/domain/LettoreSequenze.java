fd5eb7384b4d8fff77365f807adfd98ec64b3b38
marco <mar.taverniti@gmail.com>
2015-05-15 08:47:45 +0200

    bug lettura solved (2)

--- src/it/sp4te/domain/LettoreSequenze.java
+++ src/it/sp4te/domain/LettoreSequenze.java
@@ -1,9 +1,7 @@
 package it.sp4te.domain;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -24,6 +22,7 @@ public class LettoreSequenze {
 			System.out.println(segnale.values[j].pRe);
 			System.out.println(segnale.values[j].pImm);
 		}
+		scanner.close();
 	}
 	
 	public static void main (String[] args) throws FileNotFoundException, IOException{
