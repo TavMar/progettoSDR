025221af55714e7df4f3056327b73ddcd157d6ba
marco <mar.taverniti@gmail.com>
2015-05-20 13:51:27 +0200

    puppa

--- src/it/sp4te/domain/IpotesiH1.java
+++ src/it/sp4te/domain/IpotesiH1.java
@@ -11,8 +11,13 @@ public class IpotesiH1 {
 		
 		for(int i=0; i<1000000; i=i+1000){
 			blocco = new Signal(Arrays.copyOfRange(s.values, i, i+999));
+
 			System.out.println("i " + i + " energia : " + blocco.getEnergia());
 			if (blocco.getEnergia() > soglia){
+
+			System.out.println("energia "+ blocco.getEnergia());
+			if (blocco.getEnergia() > soglia)
+
 				numeroSuccessi ++;
 				System.out.println("i " + i + " energia : " + blocco.getEnergia());
 				System.out.println("successi " + numeroSuccessi);
